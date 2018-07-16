a1234a609c52d17896de26afc2d7a76103e02038
Molly Seither <molly.seither@centre.edu>
2018-07-16 04:04:35 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  smallestn = ""
+  smallestv = 10000000
+  name_hash.collect 
+  name_hash.each do |key, value|
+    if value <= smallestv
+      smallestv = value
+      smallestn = key
+    end
+  end
+  smallestna
 end
